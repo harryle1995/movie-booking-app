c4fb6e99c7b4de635076701b1d1e5cf7020df894
Harry Le <dungle090895@gmail.com>
2025-06-05 06:05:53 +0700

    Set up basic component structure with conditional views and interactivity

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <div className="h-10 bg-rose-950 flex text-amber-200 items-center justify-center">
+        Copyright
+      </div>
+    )
+}
+
+export default Footer
